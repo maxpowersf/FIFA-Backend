9ef4d30248dc89df28f911239138a3b92c923226
Paolo Marcolini <canalla88.13@gmail.com>
2020-04-04 16:11:11 -0300

    Added config to publish api

--- Ranking.API/Startup.cs
+++ Ranking.API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -33,6 +34,11 @@ namespace Ranking.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
 
             services.AddAuthorization();
             services.AddDbContext<RankingContext>(options =>
@@ -57,6 +63,15 @@ namespace Ranking.API
             }
             else
             {
+                app.Use(async (context, next) =>
+                {
+                    await next();
+                    if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                    {
+                        context.Request.Path = "/index.html";
+                        await next();
+                    }
+                });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
