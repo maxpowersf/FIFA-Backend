8b4bf2b577a550cef915da625833e22e40efd1a0
Paolo Marcolini <canalla88.13@gmail.com>
2021-09-24 16:04:53 -0300

    Added playoff matches to CurrentStandings response

--- Ranking.Domain/Enum/MatchRound.cs
+++ Ranking.Domain/Enum/MatchRound.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Ranking.Domain.Enum
 {
     public enum MatchRound
     {
+        [Description("Group")]
         Group = 1,
+        [Description("Play Off")]
         Playoff = 2,
+        [Description("Round of 16")]
         Round16 = 3,
+        [Description("Quarter Final")]
         Quarterfinal = 4,
+        [Description("Semi Final")]
         Semifinal = 5,
+        [Description("Third Place")]
         ThirdPlace = 6,
+        [Description("Final")]
         Final = 7
     }
 }
--- Ranking.Domain/Response/TournamentCurrentStandingsResponse.cs
+++ Ranking.Domain/Response/TournamentCurrentStandingsResponse.cs
@@ -7,10 +7,14 @@ namespace Ranking.Domain.Response
     public class TournamentCurrentStandingsResponse
     {
         public Tournament Tournament { get; set; }
+        public List<RoundMatches> Playoffs { get; set; }
         public List<Group> Groups { get; set; }
+        public List<RoundMatches> Rounds { get; set; }
         public TournamentCurrentStandingsResponse()
         {
+            this.Playoffs = new List<RoundMatches>();
             this.Groups = new List<Group>();
+            this.Rounds = new List<RoundMatches>();
         }
     }
 }
