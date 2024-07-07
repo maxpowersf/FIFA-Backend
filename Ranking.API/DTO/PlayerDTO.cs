b09ace1b4c636dd237618bf6c75d48eb780b8351
Paolo Marcolini <canalla88.13@gmail.com>
2024-07-07 13:11:41 +0200

    Add dev publish profile

--- Ranking.API/DTO/PlayerDTO.cs
+++ Ranking.API/DTO/PlayerDTO.cs
@@ -9,6 +9,7 @@ namespace Ranking.API.DTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string FullName { get; set; }
         public int Position { get; set; }
         public int TeamID { get; set; }
         public int Dorsal { get; set; }
